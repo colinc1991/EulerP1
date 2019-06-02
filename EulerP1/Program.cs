ce8122723241a039cc24999cb06895948a88a94d
Colin Campbell <colincampbell1991@gmail.com>
2019-06-02 14:57:35 +0100

    Add project files.

---
+++ EulerP1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace EulerP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+
+            for (int i = 0; i < 1000; i++)
+            {
+                if (IsMultiple(i))
+                {
+                    sum += i;
+                }
+            }
+
+            Console.WriteLine($"The sum of all multiples of 3 and 5 less than 1000 is {sum}");
+        }
+
+        public static bool IsMultiple(int numIn)
+        {
+            if (numIn % 3 == 0 || numIn % 5 == 0)
+            {
+                return true;
+            }
+
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
